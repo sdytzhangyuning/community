4429d1b019a9d5ba37d0c93045bfcde0955c79a7
yuning <zynsdyt@gmail.com>
2019-08-13 23:15:15 -0400

    somethings new

--- src/main/java/life/yuning/community/Controller/AuthorizeController.java
+++ src/main/java/life/yuning/community/Controller/AuthorizeController.java
@@ -1,5 +1,7 @@
 package life.yuning.community.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -27,7 +29,8 @@ public class AuthorizeController {
 	@GetMapping("/callback")
 	public String callback(
 			@RequestParam(name="code") String code,
-			@RequestParam(name="state") String state) {
+			@RequestParam(name="state") String state,
+			HttpServletRequest request) {
 		
 		AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 		
@@ -39,7 +42,11 @@ public class AuthorizeController {
 		
 		String accessToken = githubProvider.getAccessToken(accessTokenDTO);
 		GithubUser user = githubProvider.getUser(accessToken);
-		System.out.println(user.getName());
-		return "index";
+		if(user != null) {
+			request.getSession().setAttribute("user", user);
+			return "redirect:index";
+		}else {
+			return "redirect:index";
+		}
 	}
 }
